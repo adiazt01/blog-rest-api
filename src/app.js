2b5b6c5fbc2ae7f82d45648dab8d47948371de08
Armando Diaz <119072544+adiazt01@users.noreply.github.com>
2024-04-02 15:48:06 -0400

    Add postRouter to app.js

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 import express from "express";
 
 import { PrismaClient } from '@prisma/client'
+import { postRouter } from "./routes/post.route";
 
 export const prisma = new PrismaClient()
 
 export const app = express();
+
+app.use(express.json());
+
+app.use("/posts", postRouter);
